51027e6ebec7ca865ed0f9d5dedc55a31af49106
good mite <sandeepkgupta007@gmail.com>
2019-10-04 01:42:01 +0530

    fix: image click: needed to click twice previously

--- src/main.ts
+++ src/main.ts
@@ -53,20 +53,21 @@ function initEvents() {
     });
     $chatBody.addEventListener('click', ($event) => {
         removeModal();
-        let img = $event.target as HTMLElement;
+        debugger;
+        let img = $event.target as HTMLImageElement;
         if(img.classList.contains('click-to-zoom')){
             /*zoom the div  */
             // Get the modal
             const modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-            const modalImg = document.getElementById("img01");
+            const modalImg = document.getElementById("img01") as HTMLImageElement;
             const captionText = document.getElementById("caption");
-            img.onclick = function(){
+            // img.onclick = function(){
                 modal.style.display = "block";
-                modalImg.src = this.src;
-                captionText.innerHTML = this.alt;
-            }
+                modalImg.src = img.src;
+                // captionText.innerHTML = this.alt;
+            // }
 
 // Get the <span> element that closes the modal
             const span = document.getElementsByClassName("close")[0];
