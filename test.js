a31c7b0490d1b3cfc6be6a85c6903da294030afa
good mite <sandeepkgupta007@gmail.com>
2019-11-10 00:25:59 +0530

    test

--- test.js
+++ test.js
@@ -1,20 +1,19 @@
 x = {
-    "entities":
-        [{"counter": 3, "entity_id": "2", "required": false, "template_key": ""}, {
-            "counter": 3,
-            "entity_id": "7",
-            "required": false,
-            "template_key": ""
-        }, {"counter": 3, "entity_id": "4", "required": false, "template_key": ""}], "utterances":
-        [{
-            "entity": [{"entity_id": "4", "start": 2, "end": 14, "type": "custom", "value": "at can you d"}],
-            "utterance": "What can you do? What can you do? What can you do? What can you do? "
-        }, {"entity": [], "utterance": "What can you do? "}, {
-            "entity": [],
-            "utterance": "How can you help? "
-        }, {"entity": [], "utterance": "Helpp "}, {"entity": [], "utterance": "What do you do? "}], "intent_id":
-        "help", "name":
-        "Help1", "reset_state":
-        true, "template_key":
-        "help"
+    "entities": [{"counter": 3, "entity_id": "2", "required": false, "template_key": ""}, {
+        "counter": 3,
+        "entity_id": "7",
+        "required": false,
+        "template_key": ""
+    }, {"counter": 3, "entity_id": "4", "required": false, "template_key": ""}],
+    "utterances": [{
+        "entities": [{"end": 14, "entity_id": "4", "start": 2, "type": "custom", "value": "at can you d"}],
+        "utterance": "What can you do? What can you do? What can you do? What can you do? "
+    }, {"entities": [], "utterance": "What can you do? "}, {
+        "entities": [],
+        "utterance": "How can you help? "
+    }, {"entities": [], "utterance": "Helpp "}, {"entities": [], "utterance": "What do you do? "}],
+    "intent_id": "help",
+    "name": "Help1",
+    "reset_state": true,
+    "template_key": "help"
 }
