39e0bb63792239ca33299e58ad90b4f8bc055dd0
good mite <sandeepkgupta007@gmail.com>
2019-11-10 00:09:53 +0530

    test

---
+++ test.js
@@ -0,0 +1,20 @@
+x = {
+    "entities":
+        [{"counter": 3, "entity_id": "2", "required": false, "template_key": ""}, {
+            "counter": 3,
+            "entity_id": "7",
+            "required": false,
+            "template_key": ""
+        }, {"counter": 3, "entity_id": "4", "required": false, "template_key": ""}], "utterances":
+        [{
+            "entity": [{"entity_id": "4", "start": 2, "end": 14, "type": "custom", "value": "at can you d"}],
+            "utterance": "What can you do? What can you do? What can you do? What can you do? "
+        }, {"entity": [], "utterance": "What can you do? "}, {
+            "entity": [],
+            "utterance": "How can you help? "
+        }, {"entity": [], "utterance": "Helpp "}, {"entity": [], "utterance": "What do you do? "}], "intent_id":
+        "help", "name":
+        "Help1", "reset_state":
+        true, "template_key":
+        "help"
+}
