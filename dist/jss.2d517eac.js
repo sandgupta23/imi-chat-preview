73f845d0657d08aa0fe743f46910e02cc8b4682a
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2020-02-18 23:28:25 +0530

    fix: build completed

--- dist/jss.2d517eac.js
+++ dist/jss.2d517eac.js
@@ -2479,7 +2479,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52377" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60746" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
