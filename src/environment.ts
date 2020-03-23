faf7b9d8cbefcbf05c724793b6834e24e5060c5e
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2020-03-23 19:21:00 +0530

    fix: changed bot unique name to LNWH_COVID-19_FAQ

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
         bot_access_token: null,
-        bot_unique_name: "covid19",
+        bot_unique_name: "LNWH_COVID-19_FAQ",
         enterprise_unique_name: "demos",//"anirudh.t",//
         root: "",
         consumer: {
