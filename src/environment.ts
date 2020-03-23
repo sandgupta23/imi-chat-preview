08b30267d1f7840bffa1f172bbd8cc47c3d00793
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2020-03-23 19:16:58 +0530

    new branch for botuniquename covid19

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
         bot_access_token: null,
-        bot_unique_name: "carona",
+        bot_unique_name: "covid19",
         enterprise_unique_name: "demos",//"anirudh.t",//
         root: "",
         consumer: {
