133a3fa2eeaeabbc6d50d32496ed48e199a79997
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2020-05-30 22:43:04 +0530

    fix: build

--- copy-to-assets.js
+++ copy-to-assets.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
-
+var path = require('path');
+var filename1 = path.basename('\\src\\lib\\recorderWorker.js');
+var filename2 = path.basename('\\dist\\recorderWorker.js');
 // destination.txt will be created or overwritten by default.
-fs.copyFile('\\src\\lib\\recorderWorker.js', '\\dist\\recorderWorker.js', (err) => {
+fs.copyFile(filename1, filename2, (err) => {
     if (err) throw err;
     console.log('src\\lib\\recorderWorker.js was copied to dist\\recorderWorker.js');
 });
