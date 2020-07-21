5eacf73826cda55ff872f22aa7b98d8b796fda02
sandeep gupta <sandeepkumar.g@imimobile.com>
2020-07-21 17:06:37 +0530

    fix: caousel

--- dist/client.f5abc9c9.js
+++ dist/client.f5abc9c9.js
@@ -3145,7 +3145,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50342" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52198" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
