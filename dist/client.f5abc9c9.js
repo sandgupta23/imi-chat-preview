3b7b5234f8657bb291f1cb0a103589e00d7c9374
goodmite <sandeepkgupta007@gmail.com>
2020-07-30 20:55:08 +0530

    fix: width fixes

--- dist/client.f5abc9c9.js
+++ dist/client.f5abc9c9.js
@@ -422,6 +422,10 @@ function convertToLink(url, className, prefix) {
     prefix = "";
   }
 
+  if (!url) {
+    return '';
+  }
+
   var inputTextWithoutBr = url.split('<br>').join('');
 
   if (inputTextWithoutBr.includes('<') && inputTextWithoutBr.includes('>')) {
@@ -3171,7 +3175,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59844" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63883" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/response-components/link.ts
+++ src/response-components/link.ts
@@ -2,6 +2,9 @@
   * linkify: replaces all texts to <a> links in a string
   * */
 export function convertToLink(url, className = "text-link", prefix = "") {
+    if (!url) { //null causes issues
+        return '';
+    }
     const inputTextWithoutBr = url.split('<br>').join('');
     if (inputTextWithoutBr.includes('<') && inputTextWithoutBr.includes('>')) {
         return url;
@@ -11,14 +14,14 @@ export function convertToLink(url, className = "text-link", prefix = "") {
 
     // URLs starting with http://, https://, or ftp://
     replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    if(prefix)
+    if (prefix)
         replacedText = url.replace(replacePattern1, `<a href="$1" target="_blank" class="${className}">${prefix}</a>`);
     else
         replacedText = url.replace(replacePattern1, `<a href="$1" target="_blank" class="${className}">$1</a>`);
 
     // URLs starting with "www." (without // before it, or it'd re-link the ones done above).
     replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    if(prefix)
+    if (prefix)
         replacedText = replacedText.replace(replacePattern2, `$1<a href="http://$2" class="${className}" target="_blank">${prefix}</a>`);
     else
         replacedText = replacedText.replace(replacePattern2, `$1<a href="http://$2" class="${className}" target="_blank">$2</a>`);
